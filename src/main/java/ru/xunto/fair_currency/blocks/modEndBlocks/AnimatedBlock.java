6ec91eaa4620f0ae8f4e08da0824085350d3757c
Anna <an1696ns@gmail.com>
2023-07-19 03:17:52 +0300

    Add AnimatedBlock base

---
+++ src/main/java/ru/xunto/fair_currency/blocks/modEndBlocks/AnimatedBlock.java
@@ -0,0 +1,11 @@
+package ru.xunto.fair_currency.blocks.modEndBlocks;
+
+import net.minecraft.block.material.Material;
+import ru.xunto.fair_currency.blocks.BaseBlock;
+
+public class AnimatedBlock extends BaseBlock {
+
+    public AnimatedBlock(String registryName, Material material) {
+        super(registryName, material);
+    }
+}
